d89a36b5f0a2d7abc0f338c0776282c39a8241e9
shuiruge <shuiruge@hotmail.com>
2022-05-06 22:44:35 +0800

    Simplify code. Test some functions.

--- src/Lyapunov.jl
+++ src/Lyapunov.jl
@@ -1,29 +1,169 @@
-include("./StochasticDynamics.jl")
-using Random
+using Flux
 
 
-struct LyaSDE{A} <: AbstractSDE{A}
-    f;
-    T::A;
+"""
+Compute x + dx, where
+
+    dx = f(x) dt + √(2T) dW, dW ~ Normal(0, δ dt).
+
+"""
+function simulate(x, f, dt, T)
+    dW = randn(size(x)) * sqrt(dt)
+    dx = f(x) * dt .+ sqrt(2T) * dW
+    x .+ dx
 end
 
 
-function f(sde::LyaSDE, x, t)
-    sde.f(x)
+"""
+Returns the function ∇f, where
+
+  - if it's f(x), then return ∂f/∂x; and
+  - if it's f(x, y, ...), then return (∂f/∂x, ∂f/∂y, ...).
+
+"""
+function ∇(f)
+
+    function ∇f(args...)
+        if length(args) == 1
+            # If it's f(x), then return ∂f/∂x
+            gradient(Flux.sum ∘ f, args...)[1]
+        else
+            # If it's f(x, y, ...), then return (∂f/∂x, ∂f/∂y, ...)
+            gradient(Flux.sum ∘ f, args...)
+        end
+    end
+
+    ∇f
 end
 
 
-function g(sde::LyaSDE, x, t, dW)
-    √(2*sde.T) .* dW
+"""
+Random sample from Uniform(-1, 1).
+"""
+function randu(size)
+    2 * rand(Float64, size) .- 1
 end
 
 
-function normal(sde::LyaSDE, x, t)
-    randn(size(x)...)
+mutable struct Lyapunov
+    E  # the parameterized function
+    θ  # the parameter
+    ∇E  # ∂E/∂x
+    xD  # x ~ p_D
+    xE  # x ~ q_E
 end
 
 
-function simulate(f, x, dt, T)
-    sde = LyaSDE(f, T)
-    solve(sde, x, zero(dt), dt; method=EulerMethod(dt))
+function Lyapunov(E, xD, xE)
+    θ = Flux.params(E)
+    ∇E = ∇(E)
+    Lyapunov(E, θ, ∇E, xD, xE)
+end
+
+
+function Lyapunov(E, inputsize)
+    xD = randu(inputsize)
+    xE = randu(inputsize)
+    Lyapunov(E, xD, xE)
+end
+
+
+"""
+Returns ∂L/∂θ.
+"""
+function ∂L∂θ(E, θ, xD, xE)
+    # E_{p_D}[ ∂E/∂θ ]
+    ∂θpD = gradient(() -> Flux.mean(E(xD)), θ)
+
+    # E_{p_E}[ ∂E/∂θ ]
+    ∂θpE = gradient(() -> Flux.mean(E(xE)), θ)
+
+    # ∂L/∂θ
+    ∂θpD .- ∂θpE
+end
+
+
+"""
+Returns ∂L/∂θ.
+"""
+function ∂L∂θ(m::Lyapunov)
+    ∂L∂θ(m.E, m.θ, m.xD, m.xE)
+end
+
+
+"""
+Implement the learning algorithm that searches for Lyapunov function.
+"""
+function update!(
+    m::Lyapunov,
+    f,
+    dt,
+    T,
+    warmup::Bool,
+    opt::Flux.Optimise.AbstractOptimiser;
+    cb=nothing,
+)
+    m.xD .= simulate(m.xD, f, dt, T)
+    m.xE .= simulate(m.xE, (x -> -x) ∘ m.∇E, dt, T)
+
+    if warmup == false
+        gs = ∂L∂θ(m)
+        Flux.Optimise.update!(opt, m.θ, gs)
+
+        if cb !== nothing
+            cb(m, gs)
+        end
+    end
+end
+
+
+"""
+Compute ∇E ⋅ f for each sample.
+"""
+function criterion(m::Lyapunov, f, x)
+    Flux.sum(m.∇E(x) .* f(x); dims=1)[1,:]
+end
+
+
+"""
+Computes the MCMC chains. The nested dimensions, if any, are flatten,
+so that the returned `chains`, as an array, has shape:
+
+    (iterations, total_dimensions, number_of_chains)
+
+
+Parameters
+----------
+- f
+- x: Initial samples. 
+- t: Integration time.
+- dt
+- T
+
+Returns
+-------
+- chains: The input of `MCMCDiagnosticTools.gelmandiag_multivariate`.
+- x: Final samples.
+"""
+function getchains(f, x, t, dt, T)
+    function flatten_dims(x)
+        batch = size(x)[end]
+        dims = prod(size(x)[1:end-1])
+        reshape(x, (1, dims, batch))
+    end
+
+    # Initialize.
+    x = copy(x)
+    trajectory = [flatten_dims(x)]
+    τ = zero(t)
+
+    while τ < t
+        x .= simulate(x, f, dt, T)
+        push!(trajectory, flatten_dims(x))
+        τ += dt
+    end
+
+    # Each element has shape (dimensions, number_of_chains).
+    chains = cat(trajectory...; dims=1)
+    chains, x
 end
--- test/TestLyapunov.jl
+++ test/TestLyapunov.jl
@@ -1,11 +1,103 @@
 include("$(pwd())/src/Lyapunov.jl")
+using Plots
 
 
-function f(x)
-    dx = zero.(x)
-    dx[1] = x[2]
-    dx[2] = -sin(x[1]) - x[2]
+"""
+Split the array along the axis.
+"""
+function split!(array, axis)
+    [selectdim(array, axis, i) for i in 1:size(array, axis)]
+end
+
+
+function vanilla_test(x)
+    dx = zero.(x)  # initialize.
+
+    (dx₁, dx₂) = split!(dx, 1)
+    (x₁, x₂) = split!(x, 1)
+
+    @. dx₁ = -x₁
+    @. dx₂ = -x₂
+
     dx
 end
 
-@show simulate(f, [1. 0.; 2. .1], 0.1, 1.)
+
+function damped_pendulum(x)
+    dx = zero.(x)  # initialize.
+
+    (dx₁, dx₂) = split!(dx, 1)
+    (x₁, x₂) = split!(x, 1)
+
+    @. dx₁ = tanh(x₂)
+    @. dx₂ = -sin(tanh(x₁)) - tanh(x₂)
+
+    dx
+end
+
+
+
+function damped_oscillator(x)
+    dx = zero.(x)  # initialize.
+
+    (dx₁, dx₂) = split!(dx, 1)
+    (x₁, x₂) = split!(x, 1)
+
+    # Let x₁ the position of an oscillator, and x₂ the velocity.
+    @. dx₁ = x₂
+    @. dx₂ = -0.2 * x₁ - x₂
+
+    dx
+end
+
+
+# Initialize
+
+f = damped_oscillator
+E = Chain(Dense(2, 256, relu), Dense(256, 1, bias=false))
+batch = 128
+m = Lyapunov(E, (2, batch))
+opt = Flux.Optimise.RMSProp(1E-3)
+dt = 1E-1
+T = 1E-2
+train_steps = 20000
+warmup_steps = 1000
+
+xc = copy(m.xD)
+histogram(criterion(m, f, xc), bins=100)
+
+
+# Training
+
+grad_norm_history = [Float64[] for _ in m.θ]
+function history_callback(m, gs)
+    global grad_norm_history
+    for i = 1:length(gs)
+        grad_norm = Flux.mean(abs.(gs[m.θ[i]]))
+        push!(grad_norm_history[i], grad_norm)
+    end
+end
+
+for step = 1:train_steps
+    warmup = step < warmup_steps
+    update!(m, f, dt, T, warmup, opt; cb=history_callback)
+end
+
+
+# Results
+
+plot(grad_norm_history; alpha=0.5)
+histogram(criterion(m, f, xc), bins=100)
+histogram(criterion(m, f, m.xD), bins=100)
+
+
+# TODO: MCMC Convergence
+
+using MCMCDiagnosticTools
+
+x = randu((2, batch))
+for i = 1:10
+    chains, x = getchains(f, x, 2E+2, dt, T)
+    psrfmultivariate = gelmandiag_multivariate(chains).psrfmultivariate
+    @show i, psrfmultivariate
+end
