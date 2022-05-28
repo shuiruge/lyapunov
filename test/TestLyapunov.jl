d5c530ebf9b7911385aa478df54dfe41cefd9023
shuiruge <shuiruge@hotmail.com>
2022-05-28 14:56:59 +0800

    Add param_damped_oscillator. Tested.

--- test/TestLyapunov.jl
+++ test/TestLyapunov.jl
@@ -2,32 +2,22 @@ src = "$(pwd())/src"
 include("$(src)/Lyapunov.jl")
 include("$(src)/Utils.jl")
 
-using Flux
 using Plots
+using Flux: mean, Optimise
+using MCMCDiagnosticTools: gelmandiag_multivariate
+using ProgressMeter: @showprogress
+
 
 
 """
-Split the array along the axis.
+Vanilla test dynamics.
+
+Returns
+-------
+f : the vector field function.
+dim : the phase space dimension.
 """
-function split!(array, axis)
-    [selectdim(array, axis, i) for i in 1:size(array, axis)]
-end
-
-
-function vanilla_test(x)
-    dx = zero.(x)  # initialize.
-
-    (dx₁, dx₂) = split!(dx, 1)
-    (x₁, x₂) = split!(x, 1)
-
-    @. dx₁ = -x₁
-    @. dx₂ = -x₂
-
-    dx
-end
-
-
-function damped_pendulum(g, μ)
+function vanilla()
 
     function f(x)
         dx = zero.(x)  # initialize.
@@ -35,21 +25,29 @@ function damped_pendulum(g, μ)
         (dx₁, dx₂) = split!(dx, 1)
         (x₁, x₂) = split!(x, 1)
 
-        # Originally, it's
-        # @. dx₁ = x₂
-        # @. dx₂ = -g * sin(x₁) - μ * x₂
-        # But for limiting the range of x, we transform x -> atanh(x).
-        # With some additional modification, it becomes:
-        @. dx₁ = tanh(x₂)
-        @. dx₂ = -g * sin(tanh(x₁)) - μ * tanh(x₂)
+        @. dx₁ = -x₁
+        @. dx₂ = -x₂
 
         dx
     end
 
-    f
+    f, 2
 end
 
 
+"""
+Damped oscillator.
+
+Parameters
+----------
+k : the spring constant.
+μ : the damping coefficient.
+
+Returns
+-------
+f : the vector field function.
+dim : the phase space dimension.
+"""
 function damped_oscillator(k, μ)
     # Determine the criticality of the damped oscillator.
     criticality = μ^2 - 4 * k
@@ -74,24 +72,100 @@ function damped_oscillator(k, μ)
         dx
     end
 
-    f
+    f, 2
+end
+
+
+"""
+Damped oscillator with a free stiffness factor between kmin and kmax.
+
+Remark:
+    Using period linear function for k furnishes a much better results than
+    non-linear function, e.g. sigmoid function.
+
+Parameters
+----------
+kmin : the minimum of spring constant.
+kmax : the maximum of spring constant.
+μ : the damping coefficient.
+
+Returns
+-------
+f : the vector field function.
+dim : the phase space dimension.
+"""
+function param_damped_oscillator(kmin, kmax, μ)
+
+    function period_linear(x)
+        if 0 <= x <= 1
+            x
+        elseif 1 < x <= 2
+            2 - x
+        elseif x < 0
+            period_linear(x + 2)
+        else  # x > 2
+            period_linear(x - 2)
+        end
+    end
+
+    function k(z)
+        (kmax - kmin) * period_linear(z) + kmin
+    end
+
+    function f(x)
+        dx = zero.(x)  # initialize.
+
+        (dx₁, dx₂, dz) = split!(dx, 1)
+        (x₁, x₂, z) = split!(x, 1)
+
+        # Let x₁ the position of an oscillator, and x₂ the velocity.
+        @. dx₁ = x₂
+        @. dx₂ = -k(z) * x₁ - μ * x₂
+        @. dz = 0
+
+        dx
+    end
+
+    f, 3
 end
 
 
 # Initialize
 
-f = damped_oscillator(0.5, 1.0)
-E = Chain(Dense(2, 256, relu), Dense(256, 1, bias=false))
+# f, dim = damped_oscillator(0.5, 1.0)
+f, dim = param_damped_oscillator(0.1, 0.5, 1.0)
+hdim = 512
+E = Chain(
+    Dense(dim, hdim, relu),
+    Dense(hdim, 1, bias=false),
+)
 batch = 128
-m = Lyapunov(E, (2, batch))
-opt = Flux.Optimise.RMSProp(1E-3)
+m = Lyapunov(E, (dim, batch))
 dt = 1E-1
 T = 1E-2
-train_steps = 50000
-warmup_steps = 1000
+train_steps = 100000
+# Optimise.ADAM is utterly unstable, should be avoided.
+opt = Optimise.Optimiser(
+    Optimise.ClipValue(1E-1),
+    Optimise.RMSProp(1E-3),
+)
 
-xc = copy(m.xD)
-histogram(criterion(m, f, xc), bins=100)
+histogram(criterion(m, f), bins=100, title="Initial Criterion")
+
+
+# MCMC Convergence
+
+t = 10.
+x = randu((dim, batch))
+full_chains = Float64[]
+rs = Float64[]
+for i = 1:100
+    global x, full_chains, rs
+    chains, x = getchains(f, x, t, dt, T)
+    full_chains = cat(full_chains, chains; dims=1)
+    push!(rs, gelmandiag_multivariate(full_chains).psrfmultivariate)
+end
+plot(rs, title="MCMC Convergence", ylims=(0, max(rs...)))
 
 
 # Training
@@ -100,31 +174,20 @@ grad_norm_history = [Float64[] for _ in m.θ]
 function history_callback(m, gs)
     global grad_norm_history
     for i = 1:length(gs)
-        grad_norm = Flux.mean(abs.(gs[m.θ[i]]))
+        grad_norm = mean(abs.(gs[m.θ[i]]))
         push!(grad_norm_history[i], grad_norm)
     end
 end
 
-for step = 1:train_steps
-    warmup = step < warmup_steps
-    update!(m, f, dt, T, warmup, opt; cb=history_callback)
+@showprogress for step = 1:train_steps
+    cb = (step % 20 == 0) ? history_callback : nothing
+    update!(opt, m, f, dt, T; cb=cb)
 end
 
 
 # Results
 
 plot(grad_norm_history; alpha=0.5)
-histogram(criterion(m, f, xc), bins=100)
-histogram(criterion(m, f, m.xD), bins=100)
+histogram(criterion(m, f), bins=100, title="Final Criterion")
 
-
-# TODO: MCMC Convergence
-
-using MCMCDiagnosticTools
-
-x = randu((2, batch))
-for i = 1:10
-    chains, x = getchains(f, x, (warmup_steps * dt), dt, T)
-    psrfmultivariate = gelmandiag_multivariate(chains).psrfmultivariate
-    @show i, psrfmultivariate
-end
+histogram(criterion(m, f, m.x), bins=100, title="Criterion on Samples")
